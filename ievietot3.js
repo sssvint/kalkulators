b651cbb9960fdf1bf58a650ff2ef4e7e1a8df487
sssvint <93766121+sssvint@users.noreply.github.com>
2022-04-01 12:00:06 +0300

    Delete ievietot3.js

--- ievietot3.js
+++
@@ -1,6 +0,0 @@
-function ievietot3(){
-    let virkne=document.querySelector('#skaitlis').value
-    virkne=virkne+'3';
-    document.querySelector('#skaitlis').value=virkne;
-    //alert(virkne);
-    }
