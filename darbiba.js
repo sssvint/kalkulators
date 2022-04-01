8b3a76622c118b31415f120c1ded0b8b835e618f
sssvint <93766121+sssvint@users.noreply.github.com>
2022-04-01 12:00:40 +0300

    Update darbiba.js

--- darbiba.js
+++ darbiba.js
@@ -9,8 +9,67 @@ function vienads(){
     let skaitlis2=parseInt(document.querySelector('#skaitlis').value);
     document.querySelector('#skaitlis').value="";
     let skaitlis1=parseInt(document.querySelector('#skaitlis1').value);
-    if (zime=='*'){
+        if (zime=='*'){
     let rezultats=skaitlis1*skaitlis2;
     document.querySelector('#rez2').value=rezultats;
     }
+        if (zime=='+'){
+    let rezultats=skaitlis1+skaitlis2;
+    document.querySelector('#rez2').value=rezultats;
+    }
+        if (zime=='-'){
+    let rezultats=skaitlis1-skaitlis2;
+    document.querySelector('#rez2').value=rezultats;
+    }
+        if (zime=='/'){
+    let rezultats=skaitlis1/skaitlis2;
+    document.querySelector('#rez2').value=rezultats;
+    }
+        if (zime=='^'){
+    let rezultats=skaitlis1**skaitlis2;
+    document.querySelector('#rez2').value=rezultats;
+    }
+        if (zime=='v'){
+    let rezultats=Math.sqrt(skaitlis1);
+    document.querySelector('#rez2').value=rezultats;
+    }
 }
+function kapinat(){
+    let skaitlis1=document.querySelector('#skaitlis').value;
+    document.querySelector('#skaitlis').value="";
+    document.querySelector('#skaitlis1').value=skaitlis1;
+    document.querySelector('#zime').value='^';
+    document.querySelector('#skaitlis').value="";
+   }
+function kvsakne(){
+    let skaitlis1=document.querySelector('#skaitlis').value;
+    document.querySelector('#skaitlis').value="";
+    document.querySelector('#skaitlis1').value=skaitlis1;
+    document.querySelector('#zime').value='v';
+    let rezultats=Math.sqrt(skaitlis1);
+    document.querySelector('#rez2').value=rezultats;
+   }
+function sin(){
+    let skaitlis1=document.querySelector('#skaitlis').value;
+    document.querySelector('#skaitlis').value="";
+    document.querySelector('#skaitlis1').value=skaitlis1;
+    document.querySelector('#zime').value='sin';
+   }
+function saskaitit(){
+    let skaitlis1=document.querySelector('#skaitlis').value;
+    document.querySelector('#skaitlis').value="";
+    document.querySelector('#skaitlis1').value=skaitlis1;
+    document.querySelector('#zime').value='+';
+   }
+function atnemt(){
+    let skaitlis1=document.querySelector('#skaitlis').value;
+    document.querySelector('#skaitlis').value="";
+    document.querySelector('#skaitlis1').value=skaitlis1;
+    document.querySelector('#zime').value='-';
+   }
+function dalit(){
+    let skaitlis1=document.querySelector('#skaitlis').value;
+    document.querySelector('#skaitlis').value="";
+    document.querySelector('#skaitlis1').value=skaitlis1;
+    document.querySelector('#zime').value='/';
+   }
