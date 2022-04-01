b25049a4d85ecd856ab86dfb96c799db82aefd1c
sssvint <93766121+sssvint@users.noreply.github.com>
2022-04-01 12:01:01 +0300

    Update ievietot.js

--- ievietot.js
+++ ievietot.js
@@ -1,10 +1,50 @@
-function ievietot2(){
+function ievietot1(){
 let virkne=document.querySelector('#skaitlis').value
-virkne=virkne+'2';
+virkne=virkne+'1';
 document.querySelector('#skaitlis').value=virkne;
 }
+function ievietot2(){
+    let virkne=document.querySelector('#skaitlis').value
+    virkne=virkne+'2';
+    document.querySelector('#skaitlis').value=virkne;
+    }
 function ievietot3(){
     let virkne=document.querySelector('#skaitlis').value
     virkne=virkne+'3';
     document.querySelector('#skaitlis').value=virkne;
 }
+function ievietot4(){
+    let virkne=document.querySelector('#skaitlis').value
+    virkne=virkne+'4';
+    document.querySelector('#skaitlis').value=virkne;
+    }
+function ievietot5(){
+    let virkne=document.querySelector('#skaitlis').value
+    virkne=virkne+'5';
+    document.querySelector('#skaitlis').value=virkne;
+    }
+function ievietot6(){
+let virkne=document.querySelector('#skaitlis').value
+    virkne=virkne+'6';
+    document.querySelector('#skaitlis').value=virkne;
+    }
+function ievietot7(){
+let virkne=document.querySelector('#skaitlis').value
+virkne=virkne+'7';
+document.querySelector('#skaitlis').value=virkne;
+}
+function ievietot8(){
+    let virkne=document.querySelector('#skaitlis').value
+    virkne=virkne+'8';
+    document.querySelector('#skaitlis').value=virkne;
+    }
+function ievietot9(){
+    let virkne=document.querySelector('#skaitlis').value
+    virkne=virkne+'9';
+    document.querySelector('#skaitlis').value=virkne;
+    }
+    function ievietot0(){
+    let virkne=document.querySelector('#skaitlis').value
+    virkne=virkne+'0';
+    document.querySelector('#skaitlis').value=virkne;
+    }
